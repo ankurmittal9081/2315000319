62f7e65cb46f842fee4d02a5367c55849714bc06
Ankur Mittal <ankurmittal9081@gmail.com>
2026-06-10 13:19:05 +0530

    Initialize Stage 2 React application

---
+++ stage2/src/pages/PriorityNotifications.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PriorityNotifications() {
+  return (
+    <div>
+      <h1>Priority Notifications</h1>
+    </div>
+  );
+}
+
+export default PriorityNotifications;
